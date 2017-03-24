7b41308e0b4cb031f2e37b5a7712f900f3b2bdef
daose <bychen@uwaterloo.ca>
2017-03-24 07:35:05 -0400

    use listview instead of scrollview

--- src/components/Menu/MenuComponent.js
+++ src/components/Menu/MenuComponent.js
@@ -1,35 +1,58 @@
-import React from 'react';
-import { ScrollView, Button, View } from 'react-native';
+import React, { Component } from 'react';
+import { ListView, Button, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import styles from './styles';
 
-const MenuComponent = () => (
-  <ScrollView style={styles.container}>
-    <View style={styles.button}>
-      <Button
-        onPress={Actions.LOGIN}
-        title="Login Form"
-      />
-    </View>
-    <View style={styles.button}>
-      <Button
-        onPress={Actions.REDDIT}
-        title="Network Request"
-      />
-    </View>
-    <View style={styles.button}>
-      <Button
-        onPress={Actions.TABS}
-        title="Tabs"
-      />
-    </View>
-    <View style={styles.button}>
-      <Button
-        onPress={Actions.MAPS}
-        title="Maps"
-      />
-    </View>
-  </ScrollView>
-);
+const demos = [
+  {
+    title: 'Login Form',
+    action: 'LOGIN'
+  },
+  {
+    title: 'Network Request',
+    action: 'REDDIT'
+  },
+  {
+    title: 'Tabs',
+    action: 'TABS'
+  },
+  {
+    title: 'Maps',
+    action: 'MAPS'
+  }
+];
 
-export default MenuComponent;
+export default class MenuComponent extends Component {
+  static renderRow({ title, action }) {
+    return (
+      <View style={styles.button}>
+        <Button
+          title={title}
+          onPress={Actions[action]}
+        />
+      </View>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows(demos)
+    };
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return (
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={MenuComponent.renderRow}
+      />
+    );
+  }
+}
